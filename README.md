da407f25301e78cb4208ace44f5867bedd64825d
SpencerrrC <spencerchin2004@gmail.com>
2025-03-28 15:40:19 -0400

    Create div generator function for width

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # EtchASketch
-- Function to create div "n" number of times, use css grid. 960px wide
+- Function to create div "n" number of times
+- 960px wide, flexbox divvies up the space evenly
 - Hover trigger DOM effect
+
+Enter width, then the height number will be the number of repeated times to print rows
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const sizeRaw = document.getElementById("sizeForm").addEventListener("submit", function (event){
-    
+document.querySelector("form").addEventListener("submit", function (e){
+    e.preventDefault();
+    const inputField = document.querySelector("input");
+    const numSize = Number(inputField.value);
+    if (!isNaN(numSize) && numSize > 0 && numSize < 100) {
+        grid(numSize);
+    } else {
+        console.error("Please enter a valid number.");
+    }
 });
-const inputNumber = Number(sizeRaw); 
+
 function grid(size) {
-    container = document.querySelector("#container");
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
     for (let i=0; i < size; i++) {
         const tile = document.createElement("div");
         container.appendChild(tile);
         tile.className = "tile";
+        tile.innerText = "hi";
         console.log("tile");
+        localStorage.setItem(`tile-${i}`, "created");
     }
 } 
-console.log(grid(inputNumber));
+
