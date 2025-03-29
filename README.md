cbbc9967d903f0b3f8b4ac1e5b68a46154de3a7c
SpencerrrC <spencerchin2004@gmail.com>
2025-03-29 14:57:28 -0400

    Create box for container

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # EtchASketch
-- Function to create div "n" number of times
+<!-- - Function to create div "n" number of times -->
 - 960px wide, flexbox divvies up the space evenly
 - Hover trigger DOM effect
-
-Enter width, then the height number will be the number of repeated times to print rows
--- index.js
+++ index.js
@@ -1,24 +1,76 @@
-document.querySelector("form").addEventListener("submit", function (e){
-    e.preventDefault();
-    const inputField = document.querySelector("input");
-    const numSize = Number(inputField.value);
-    if (!isNaN(numSize) && numSize > 0 && numSize < 100) {
-        grid(numSize);
-    } else {
-        console.error("Please enter a valid number.");
-    }
-});
+// POPUP PROMPT
+function userPrompt() {
+let boxWidth= prompt("Enter a desired size", 10);
+let numWidth= Number(boxWidth);
+grid(numWidth);
+console.log(typeof(numWidth));
+}
 
 function grid(size) {
-    const container = document.querySelector("#container");
+    container = document.querySelector("#container");
     container.innerHTML = "";
-    for (let i=0; i < size; i++) {
-        const tile = document.createElement("div");
-        container.appendChild(tile);
-        tile.className = "tile";
-        tile.innerText = "hi";
-        console.log("tile");
-        localStorage.setItem(`tile-${i}`, "created");
+    for (i=0; i<size; i++) {
+            for (j=0; j<size; j++){
+            const tile = document.createElement("div");
+            tile.className = "tile";
+            tile.innerText = "hi";
+            container.appendChild(tile);
+            console.log("tile");
+            localStorage.setItem(`tile-${i}`, "created");
+        }
     }
-} 
+}
+
+// GENERAL DOCUMENT CODE
+userPrompt();
+const resetButton = document.querySelector("#reset").addEventListener("click", () => {
+    userPrompt();
+})
+
+// CREATING DIV
+
+
+// document.querySelector("#sizeForm").addEventListener("submit", function (e){
+//     e.preventDefault();
+    
+//     const inputField = document.querySelector("input");
+//     const numSize = Number(inputField.value);
+//     if (!isNaN(numSize) && numSize > 0 && numSize < 100) {
+//         grid(numSize);
+//     } else {
+//         console.error("Please enter a valid number.");
+//     }
+// });
+
+// function grid(size) {
+//     const container = document.querySelector("#container");
+//     container.innerHTML = "";
+//     for (let i=0; i < size; i++) {
+//         const tile = document.createElement("div");
+//         container.appendChild(tile);
+//         tile.className = "tile";
+//         tile.innerText = "hi";
+//         console.log("tile");
+//         localStorage.setItem(`tile-${i}`, "created");
+//     }
+// } 
+
+// document.querySelector("#height").addEventListener("submit", function (e){
+//     e.preventDefault();
+//     const inputField = document.querySelector("input");
+//     const numSize = Number(inputField.value);
+//     if (!isNaN(numSize) && numSize > 0 && numSize < 80) {
+//         grid(numSize);
+//     } else {
+//         console.error("Please enter a valid number.");
+//     }
+// });
+
+// function height(input) {
+//     document.querySelector("#container")
+
+//     for (i=1;i<input;i++) {
+//         grid(numSize);
+//     }
+// }
 
