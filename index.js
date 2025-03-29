6875d7a8f49f47a5fe49e45597e43540a5a0e640
SpencerrrC <spencerchin2004@gmail.com>
2025-03-29 15:47:49 -0400

    Fix row error

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 // POPUP PROMPT
 function userPrompt() {
-let boxWidth= prompt("Enter a desired size", 10);
+let boxWidth= prompt("Enter a desired size", 6);
 let numWidth= Number(boxWidth);
 grid(numWidth);
 console.log(typeof(numWidth));
 }
 
 function grid(size) {
-    container = document.querySelector("#container");
+    const container = document.querySelector("#container");
     container.innerHTML = "";
     for (i=0; i<size; i++) {
         const row = document.createElement("div"); 
         row.className = "row";
+        container.appendChild(row);
+        console.log(row);
             for (j=0; j<size; j++){
             const tile = document.createElement("div");
             tile.className = "tile";
             tile.innerText = "hi";
-            container.appendChild(tile);
+            row.appendChild(tile);
             console.log("tile");
             localStorage.setItem(`tile-${i}`, "created");
         }
